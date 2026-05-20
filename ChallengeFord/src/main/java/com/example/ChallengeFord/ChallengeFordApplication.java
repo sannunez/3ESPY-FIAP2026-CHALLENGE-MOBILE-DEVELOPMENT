2ceb1d2b746eee8112ceedf0744eaaafe2578d37
unknown <sftwr.gsantosnunes@gmail.com>
2026-05-19 21:54:50 -0300

    Fixing Error in Application

--- ChallengeFord/src/main/java/com/example/ChallengeFord/ChallengeFordApplication.java
+++ ChallengeFord/src/main/java/com/example/ChallengeFord/ChallengeFordApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class gChallengeFordApplication {
+public class ChallengeFordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeFordApplication.class, args);
